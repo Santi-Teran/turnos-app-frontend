62b4ae12d4a0d4103293c7e118f6c35b26e9e3e5
Santi-Teran <santiagoteranmatias@gmail.com>
2024-06-06 16:01:09 -0300

    cambio url api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const API_URL = 'http://localhost:5139/api';
+const API_URL = 'https://turnosappbackend.azurewebsites.net/api';
 
 export const fetchServices = async (token) => {
   try {
